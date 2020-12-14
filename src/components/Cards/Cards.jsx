10f0472f876a1d3666d60e69e1c1b6908e029445
Kerry L. Myles <kmyles@mdd.com>
2020-12-13 21:46:27 -0600

    added styling for Cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import cx from 'classnames'
 import CountUp from 'react-countup'
 import styles from './Cards.module.css'
 
@@ -11,7 +12,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -26,7 +27,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
                         <Typography variant="body2">Number of active cases of Covid-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -41,7 +42,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
                         <Typography variant="body2">Number of recoveries from  Covid-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
